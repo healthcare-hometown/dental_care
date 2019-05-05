27b69e5dcb22730f9722a95efee63d29983b0e1b
staidala <s.r.taidala@gmail.com>
2019-05-05 11:21:58 +0530

    Revert "filemovement.sh"
    This reverts commit 62e23ca7d383ce9f2875ce8f96726faf5a38d9dd.

--- filemovement.sh
+++ filemovement.sh
@@ -7,6 +7,4 @@ this is second program in git
 
 will do many more
 
-Am going add few new things to make change
-
-this is not working , will check later once
+Am going add few new things to make changes
